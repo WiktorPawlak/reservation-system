d5f82ec91eebfcae1e5780ac40ec6470a273a077
236621 Wiktor Pawlak <236621@edu.p.lodz.pl>
2022-04-18 16:24:55 +0200

    Establish repository

---
+++ MultiplexReservationSystem/src/main/java/p/lodz/pl/multiplexreservationsystem/MultiplexReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package p.lodz.pl.multiplexreservationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiplexReservationSystemApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MultiplexReservationSystemApplication.class, args);
+  }
+
+}
